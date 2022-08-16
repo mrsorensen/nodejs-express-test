4642da7bca9efbddbf6996b33a08812fb1fa1f4d
mrsorensen <40432547+mrsorensen@users.noreply.github.com>
2022-08-16 12:15:00 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.json({ message: 'Working' })
+  res.json({ message: Math.floor(Math.random() * 10000) })
 })
 
 app.listen(8080, () => {
